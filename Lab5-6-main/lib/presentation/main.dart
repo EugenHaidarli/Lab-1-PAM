5e4fce8efb55ef9e4b6fbdc79e0045f38125444f
bigtittygothgf <72318683+EugenHaidarli@users.noreply.github.com>
2023-01-09 14:00:08 +0200

    Lab 5-6

---
+++ Lab5-6-main/lib/presentation/main.dart
@@ -0,0 +1,57 @@
+
+import 'package:flutter/material.dart';
+import 'package:three_pam/presentation/pages/news.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Flutter Demo',
+    home: MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 32.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SecondRoute()),
+                  );
+                },
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(
+                      const Color.fromRGBO(0, 0, 0, 1)),
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.fromLTRB(130, 12, 130, 12)),
+                ),
+                child: const Text('Start'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
